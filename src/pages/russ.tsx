b61b957f839f40ef5ea045d09d6ecc419ca4252f
Russ MacShane <rmacshane@liquidweb.com>
2022-07-14 13:05:23 -0500

    Show a Styled Div using MUI Styled

--- src/pages/russ.tsx
+++ src/pages/russ.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
+import { styled } from "@mui/material/styles";
 import Test from "../components/basic/test";
 import RussButton from "../components/basic/russ-button";
+
+const StyledDiv = styled('div')`
+  margin-top: 20px;
+`;
 
 const RussTest = () => (
   <main>
     <h1>Russ Test</h1>
     <p>This page is a simple test!</p>
     <Test input="RUSSELL" />
-    <div style={{ marginTop: "20px" }}>
+    <StyledDiv>
       <RussButton text="Russ Button" />
-    </div>
+    </StyledDiv>
   </main>
 );
 
