099a5629713ab507fff85fde3dd8c3d768986a42
Russ MacShane <rmacshane@liquidweb.com>
2022-07-14 13:26:01 -0500

    Add tooltip

---
+++ src/components/basic/russ-tooltip/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Tooltip from "@mui/material/Tooltip";
+
+const StyledTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))`
+  & .MuiTooltip-arrow {
+    color: #0f1b45;
+  }
+
+  & .MuiTooltip-tooltip {
+    background-color: #0f1b45;
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: normal;
+    max-width: 220px;
+  }
+`;
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+};
+
+const RussTooltip = ({ children, title }: Props) => {
+  return (
+    <>
+      <StyledTooltip title={title} arrow placement="top" enterTouchDelay={0}>
+        <span className="underline">{children}</span>
+      </StyledTooltip>
+    </>
+  );
+};
+
+export default RussTooltip;
--- src/pages/russ.tsx
+++ src/pages/russ.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Test from "../components/basic/test";
 import RussButton from "../components/basic/russ-button";
+import RussTooltip from "../components/basic/russ-tooltip";
 
-const StyledDiv = styled('div')`
+const StyledDiv = styled("div")`
   margin-top: 20px;
 `;
 
@@ -14,6 +15,9 @@ const RussTest = () => (
     <Test input="RUSSELL" />
     <StyledDiv>
       <RussButton text="Russ Button" />
+    </StyledDiv>
+    <StyledDiv>
+      <RussTooltip title="Russ Tooltip Test">Tooltip Test Text</RussTooltip>
     </StyledDiv>
   </main>
 );
