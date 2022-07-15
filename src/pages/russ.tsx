2d64901b8b81e72ed7491261e5023314cb0bd3f7
Russ MacShane <rmacshane@liquidweb.com>
2022-07-15 14:27:39 -0500

    More styling

--- src/pages/russ.tsx
+++ src/pages/russ.tsx
@@ -9,30 +9,33 @@ import { styled } from "@mui/material/styles";
 const StyledParagraph = styled("div")`
   font-weight: bold;
   font-size: larger;
-  border: 1px solid red;
+  border: 1px solid DARKSLATEBLUE;
 `;
 
 const RussTest = () => (
   <Container>
-    <h1>Russ Test</h1>
+    <Box component="h1" sx={{fontSize: "xxx-large", fontFamily: "Monospace"}}>Russ Test</Box>
 
-    <Stack spacing={3} alignItems="flex-start">
-      <StyledParagraph>This page is a simple test! (using Styled Components)</StyledParagraph>
-      <Box sx={{ fontWeight: "bold", fontSize: "larger", border: "1px solid red" }}>
-        This page is a simple test! (Using Box and sx)
-      </Box>
+    <Stack direction={{ xs: "column", sm: "row" }} alignItems="stretch">
+      <Stack spacing={3} alignItems="flex-start" sx={{ width: ["100%", "50%"] }}>
+        <StyledParagraph>This page is a simple test! (using Styled Components)</StyledParagraph>
+        <Box sx={{ fontWeight: "bold", fontSize: "larger", border: "1px solid DARKSLATEBLUE" }}>
+          This page is a simple test! (Using Box and sx)
+        </Box>
 
-      <Stack spacing={2} alignItems="flex-start">
-        <Test input="RUSSELL" />
-        <RussButton text="Russ Button" />
-        <RussTooltip title="Russ Tooltip Test">Tooltip Test Children!!</RussTooltip>
+        <Stack spacing={2} alignItems="flex-start">
+          <Test input="RUSSELL" />
+          <RussButton text="Russ Button" />
+          <RussTooltip title="Russ Tooltip Test">Tooltip Test Children!!</RussTooltip>
+        </Stack>
+
+        <Stack direction={{ xs: "column", sm: "row" }} spacing={{ xs: 1, sm: 2, md: 4 }}>
+          <img src={Icon} alt="" width={50} />
+          <img src={Icon} alt="" width={50} />
+          <img src={Icon} alt="" width={50} />
+        </Stack>
       </Stack>
-
-      <Stack direction={{ xs: "column", sm: "row" }} spacing={{ xs: 1, sm: 2, md: 4 }}>
-        <img src={Icon} alt="" width={50} />
-        <img src={Icon} alt="" width={50} />
-        <img src={Icon} alt="" width={50} />
-      </Stack>
+      <Stack sx={{ backgroundColor: "DARKSLATEBLUE", width: "50%" }} alignItems="stretch" />
     </Stack>
   </Container>
 );
